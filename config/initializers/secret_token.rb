b73947cfd5ed7ed8a2a5011e9ca9450442188446
Dan Buettner <dbuettner@merchantsbonding.com>
2012-10-01 19:26:00 -0500

    initial commit of bare rails app with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterHappiness::Application.config.secret_token = '1ffef6f7dfb67b02481f3531295e4112703bb6d90e038da1201a2cc1bd7929dfe13dcbd7eefff02f199e3906952e5ff6de8708b0fe00ed3d4a2cd09418e4b2d0'
