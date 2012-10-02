a4a93713935f51a223776d0f20af8cb49421a843
Dan Buettner <dbuettner@merchantsbonding.com>
2012-10-01 19:43:54 -0500

    commit tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121002003804) do
+ActiveRecord::Schema.define(:version => 20121002003902) do
 
   create_table "cities", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "tweets", :force => true do |t|
+    t.string   "content"
+    t.integer  "happiness"
+    t.integer  "city_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
