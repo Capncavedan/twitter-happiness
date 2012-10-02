1bb5b9cfd23b16ccfb089993b0b3363b30bac8c2
Dan Buettner <dbuettner@merchantsbonding.com>
2012-10-01 21:57:34 -0500

    add view spec to ensure city name and score render

--- spec/views/cities/index.html.erb_spec.rb
+++ spec/views/cities/index.html.erb_spec.rb
@@ -7,4 +7,11 @@ describe "/cities/index" do
     render
   end
 
+  it "should display a city name" do
+    city1 = stub_model(City, name: 'City One', happiness: 55)
+    @cities = [city1]
+    render
+    rendered.should =~ /City One \(score: 55\)/
+  end
+
 end
